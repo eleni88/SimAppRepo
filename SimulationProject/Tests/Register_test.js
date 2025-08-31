d6d2144041442f7b4271a11c982f841ef9442cb7
eleni88 <papaleni88@hotmail.com>
2025-09-01 02:23:13 +0300

    test 1

---
+++ SimulationProject/Tests/Register_test.js
@@ -0,0 +1,81 @@
+﻿import http from 'k6/http';
+import { sleep } from 'k6';
+import { check } from 'k6';
+import exec from 'k6/execution';
+
+export const options = {
+    scenarios: {
+        register_scenario: {
+            // name of the executor to use
+            executor: 'per-vu-iterations',
+
+            gracefulStop: '5s',
+
+            vus: 50,
+            iterations: 1,
+            maxDuration: '2m',
+        },
+    },
+    thresholds: {
+        http_req_failed: ['rate<0.01'],   // http errors should be less than 1%      
+        http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms   
+    },
+};
+
+const RUN_ID = __ENV.RUN_ID 
+
+export default function () {
+    const i = exec.vu.idInTest;
+    const username = `user${i}_${RUN_ID}`;
+    const password = `User${i}Password!@#123*${RUN_ID}`;
+    const firstname = `User${i}`;
+    const lastname = `User${i}`;
+    const email = `${username}@example.com`;
+    const admin = false;
+    const age = 30;
+    const jobtitle = 'Developer';
+    const active = true;
+    const organization = 'Lotr Corp';
+    const securityquestion = 'What city were you born in?';
+    const securityanswer = `Athens${i}!@#123`;
+    const securityquestion1 = 'What was the first concert you attended?';
+    const securityanswer1 = `Scorpions${i}!@#123`;
+    const securityquestion2 = 'What was the make and model of your first car?';
+    const securityanswer2 = `Corsa${i}!@#123`;
+
+    const payload = JSON.stringify({
+        username: username,
+        password: password,
+        firstname: firstname,
+        lastname: lastname, 
+        email: email,
+        admin: admin,
+        age: age,
+        jobtitle: jobtitle,
+        active: active,
+        organization: organization,
+        securityQuestion: securityquestion,
+        securityAnswer: securityanswer,
+        securityQuestion1: securityquestion1,
+        securityAnswer1: securityanswer1,
+        securityQuestion2: securityquestion2,
+        securityAnswer2: securityanswer2,       
+    });
+
+    const userregister = http.post('https://localhost:7121/api/Ath/register',
+        payload,
+        { headers: { 'Content-Type': 'application/json' } }
+    );
+
+    if (userregister.status !== 200 && userregister.status !== 201) {
+        console.error('Register failed:', userregister.status);
+        console.error('Resp headers:', JSON.stringify(userregister.headers));
+
+        console.error('Body:', String(userregister.body).slice(0, 500));
+    }
+
+    check(userregister, { 'register success': (r) => r.status === 201 || r.status === 200,
+    });
+
+    sleep(1);
+}
--- SimulationProject/Tests/login_test.js
+++ SimulationProject/Tests/login_test.js
@@ -1,34 +1,60 @@
 ﻿import http from 'k6/http';
 import { sleep } from 'k6';
 import { check } from 'k6';
+import exec from 'k6/execution';
 
 export const options = {
     vus: 50,
-    duration: '5m',
+    duration: '2m',
 };
 
-// array of users
-const users = [];
-for (let i = 1; i <= 50; i++) {
-    users.push({
-        username: `user${i}`,
-        password: `User${i}Password!@#123`
-    });
-}
+const RUN_ID = __ENV.RUN_ID 
 
 // The default exported function is gonna be picked up by k6 as the entry point for the test script. 
 // It will be executed repeatedly in "iterations" for the whole duration of the test.
 export default function () {
-    // Login URL
-    const user = users[(__VU - 1) % users.length];
+    const i = exec.vu.idInTest - 1;
 
-    const res = http.post('https://127.0.0.1:5500/api/ath/login',
-        JSON.stringify({ username: user.username, password: user.password }),
+    const username = `user${i}_${RUN_ID}`;
+    const password = `User${i}Password!@#123*${RUN_ID}`;
+
+    //1. Login
+    const userlogin = http.post('https://localhost:7121/api/Ath/login',
+        JSON.stringify({ username, password }),
+        { headers: { 'Content-Type': 'application/json' } }
+    );
+    // makes sure the HTTP response code is a 200
+    check(userlogin, { 'is status 200': (r) => r.status === 200,
+    });
+
+    //2. Get Users
+    const getusers = http.get('https://localhost:7121/api/users');
+    check(getusers, { 'GET 200': (r) => r.status === 200 });
+
+    //3. Create Simulation
+    const newSimulation = {
+        Name: 'sim10',
+        Description: 'sim10', 
+        Codeurl: 'https://github.com/eleni88/MasterSlaveSimulation',
+        Simparams:  '{ "simulations": [ { "interArrivalTime": 1.0, "serviceTime": 2.0, "serverCapacity": 1, },{ "interArrivalTime": 1.5, "serviceTime": 1.0, "serverCapacity": 2, "queueCapacity": 10, "duration": 40 } ] }',
+        Simcloud: 1,
+        Regionid: 1,
+        Resourcerequirement: {
+            Instancetype: 'Pod',
+            Mininstances: 1,
+            Maxinstances: 5
+        },
+        Simuser: user,
+        Createdate: new Date().toISOString()
+    };
+
+    const simulationcreate = http.post('https://localhost:7121/api/simulations/create',
+        JSON.stringify(newSimulation),
         { headers: { 'Content-Type': 'application/json' } }
     );
 
-    // makes sure the HTTP response code is a 200
-    check(res, { 'is status 200': (r) => r.status === 200,
+    check(simulationcreate, {
+        'POST created': (r) => r.status === 201 || r.status === 200,
     });
 
     // Sleep for 1 second to simulate real-world usage
