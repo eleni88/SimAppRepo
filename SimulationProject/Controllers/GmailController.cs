0a4a1ad6af9db09ee6012d639fffe8de20bb7fbe
eleni88 <papaleni88@hotmail.com>
2025-11-30 20:29:56 +0200

    !

--- SimulationProject/Controllers/GmailController.cs
+++ SimulationProject/Controllers/GmailController.cs
@@ -46,35 +46,6 @@ namespace SimulationProject.Controllers
             {
                 return StatusCode(500, $"Failed to send email: {ex.Message}");
             }
-
-            //var accessToken = await _tokenService.GetAccessTokenAsync();
-
-            //// Compose MIME email
-            //var mimeMessage = new StringBuilder();
-            //mimeMessage.AppendLine($"To: {userEmail}");
-            //mimeMessage.AppendLine("Your temporary code is: ");
-            //mimeMessage.AppendLine("Content-Type: text/plain; charset=utf-8");
-            //mimeMessage.AppendLine();
-            //mimeMessage.AppendLine(user.Tempcode);
-
-            //var raw = Convert.ToBase64String(Encoding.UTF8.GetBytes(mimeMessage.ToString()))
-            //            .Replace("+", "-").Replace("/", "_").Replace("=", "");
-
-            //var jsonPayload = JsonSerializer.Serialize(new { raw });
-            //var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
-
-            //using var client = new HttpClient();
-            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-
-            //var response = await client.PostAsync(, content);
-            //var responseBody = await response.Content.ReadAsStringAsync();
-
-            //if (response.IsSuccessStatusCode)
-            //{
-            //    return Ok(new { message = "A temporary password has been sent to", to = userEmail });
-            //}
-
-            //return StatusCode((int)response.StatusCode, new { error = responseBody });
         }
 
     }
